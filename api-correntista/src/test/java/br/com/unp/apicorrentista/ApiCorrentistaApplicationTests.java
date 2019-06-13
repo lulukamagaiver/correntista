b7f4f843f3665d52795d2d308471f3da440eb7f0
Marcus Vinicius G. Oliveira <luluka.magaiver@gmail.com>
2019-06-13 00:39:04 -0300

    #4 - criado a api

---
+++ api-correntista/src/test/java/br/com/unp/apicorrentista/ApiCorrentistaApplicationTests.java
@@ -0,0 +1,19 @@
+package br.com.unp.apicorrentista;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ApiCorrentistaApplicationTests {
+
+	@Test
+	public void contextLoads() throws IOException, URISyntaxException {
+	}
+
+}
